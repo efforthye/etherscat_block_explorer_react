fd533af4b88a739af14afd030505d6b79d1d64cb
efforthye <efforthye@gmail.com>
2023-02-23 17:29:34 +0900

    Update AllBlock.jsx

--- web/src/components/AllBlock.jsx
+++ web/src/components/AllBlock.jsx
@@ -3,6 +3,8 @@ import styled from "styled-components";
 
 const AllBlockComponent = ({ blocks, allPageNum, setPage, page }) => {
 
+    console.log(blocks);
+
     // 페이지 배열
     const pagenation = (allPageNum) => {
         const pageArr = [];
@@ -12,27 +14,49 @@ const AllBlockComponent = ({ blocks, allPageNum, setPage, page }) => {
         return pageArr;
     }
 
-    const pagePlus = () => {
-        setPage(page + 1);
-        return page;
-    }
+
+    // 스크롤을 올리는 함수
+    const moveTop = () => {
+        window.scrollTo({
+            top: 100,
+            behavior: "smooth",
+        });
+    };
+
 
     return (
         <>
             <AllWrap>
+                {/* 현재 블록 넘버 구해서 올리기 */}
+                <Title>Blocks</Title>
 
-                {blocks?.map((block, index) => (
-                    <BlockWrap key={`block-${index}`}>
-                        <BlockInfo>{block?.number} : {block?.hash}</BlockInfo>
-                    </BlockWrap>
-                ))}
+                {/* 테이블로 바꾸기 */}
+                <BlocksWrap>
+                    <InfoWrap>
+                        {/* <Information>
+                            <span>ㅇㅅㅇ</span>
+                            <span>ㅇㅅㅇ</span>
+                            <span>ㅇㅅㅇ</span>
+                        </Information> */}
+                    </InfoWrap>
+                    {/* 하나하나의 블록 정보 */}
+                    {blocks?.map((block, index) => (
+                        <BlockWrap key={`block-${index}`}>
+                            <BlockInfo>
+                                <span>{block?.number}</span>
+                                <span>{block?.hash}</span>
+                                <span>{block?.miner}</span>
+                            </BlockInfo>
+                        </BlockWrap>
+                    ))}
+                </BlocksWrap>
 
                 {/* 페이지 출력하는 함수 : 이전, 다음 만들기 */}
-                <LinkWrap>
-
+                <PageWrap>
                     <LinkDiv>
                         <Link to={`/allBlock/1`} onClick={() => {
                             setPage(1);
+                            moveTop();
                         }}>처음</Link>
                     </LinkDiv>
                     <LinkDiv>
@@ -60,9 +84,10 @@ const AllBlockComponent = ({ blocks, allPageNum, setPage, page }) => {
                     <LinkDiv>
                         <Link to={`/allBlock/${allPageNum}`} onClick={() => {
                             setPage(allPageNum);
+                            moveTop();
                         }}>마지막</Link>
                     </LinkDiv>
-                </LinkWrap>
+                </PageWrap>
 
             </AllWrap>
         </>
@@ -72,26 +97,60 @@ const AllBlockComponent = ({ blocks, allPageNum, setPage, page }) => {
 export default AllBlockComponent;
 
 const AllWrap = styled.div`
-    margin: 0 auto;
     width: 70%;
+    margin: 0 auto;
+    padding: 30px;
+    box-sizing: border-box;
+    background-color: #fff;
+    box-shadow: 0 2px 5px 1px rgb(64 60 67 / 20%);
+    border-radius: 10px;
+    margin-top: 45px;
+`;
+const Title = styled.div`
+    font-size: 18px;
+    font-weight: 600;
+    border-bottom: 1px solid #E9ECEF;
+    padding: 20px 0;
+    &>span{
+        font-size: 15px;
+        color: #6C757D;
+        font-weight: 500;
+    }
+`;
+const BlocksWrap = styled.div`
+
+`;
+const BlockWrap = styled.div`
+    margin: 30px 0;
+`;
+const InfoWrap = styled.div`
+
+`;
+// const Information = styled.div`
+//     padding-right: 50px;
+//     display: flex;
+//     justify-content: space-between;
+// `;
+const BlockInfo = styled.div`
+    overflow: hidden;
+    text-overflow: ellipsis;
+    &>span{
+        margin-right: 20px;
+    }
 `;
 const LinkDiv = styled.div`
     display: inline-block;
     margin: 5px;
     &>a{
-        color: black;
+        color: #2c2c2c;
         text-decoration: none;
+        font-weight: 600;
     }
 `;
-const LinkWrap = styled.div`
+const PageWrap = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
     padding: 10px 0;
-`;
-const BlockWrap = styled.div`
-
-`;
-const BlockInfo = styled.div`
-
+    margin-top: 20px;
 `;
